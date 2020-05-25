435aef22da99d1580375ddbb6cf8ae7a95e26e8e
chaballen <chab.a.allen@gmail.com>
2020-05-24 23:04:11 -0500

    updated volunteer page

--- app/src/about/volunteer/hoby.js
+++ app/src/about/volunteer/hoby.js
@@ -43,33 +43,48 @@ class Hoby extends Component {
                     <div className={'column'}>
                         <div className={'head'}>
                             <h4>Community Leadership Workshop (CLeW) Coordinator</h4>
-                            <h5>August 2018 - May 2019</h5>
+                            <h5>August 2015 - December 2015</h5>
                         </div>
                         <p>
-                            As a representative of my class in Hanszen's student government, I:
+                            As a facilitator at HOBY NM, I received a scholarship to attend
+                            HOBY's Advanced Leadership Academy (ALA), where I learned how to
+                            put my leadership skills to work through helping others. After
+                            returning from ALA, a fellow student and I hosted Roswell's first
+                            CLeW, a one day seminar to jumpstart high school freshmen's
+                            interest in leadership. As a CLeW Coordinator, I:
                             <ul>
-                                <li>Planned class-oriented events to facilitate bonding and culture.</li>
-                                <li>Gathered opinions on student-government decisions and issues from
-                                    other juniors.</li>
-                                <li>Voted in line with juniors' opinions at government cabinet meetings.</li>
+                                <li>Planned and hosted a program consisting of community service, teamwork,
+                                    and leadership activities based on the HOBY curriculum.</li>
+                                <li>Corresponded with various businesses to secure a venue for the workshop
+                                    and donations for shirts, food, and other supplies.</li>
+                                <li>Managed the budget and documented details of the itinerary, food/drink
+                                    sources, layout of the event, volunteers, and attendees.</li>
+                                <li>Recruited and oversaw volunteers for facilitation, recruitment, and
+                                    operations.</li>
+                                <li>Corresponded with schools in Roswell and surrounding areas to recruit
+                                    student ambassadors.</li>
                             </ul>
                         </p>
                     </div>
                     <div className={'column'}>
                         <div className={'head'}>
                             <h4>HOBY-NM Facilitator</h4>
-                            <h5>August 2017 - May 2018</h5>
+                            <h5>May 2015, May 2016</h5>
                         </div>
                         <p>
-                            <a id={'inline'}
-                               href={'https://riceprogramcouncilsite.wordpress.com/'}>RPC</a>
-                            is an organization at Rice that coordinates various events throughout
-                            the year, such as our fall and spring dances. In this position, I:
+                            HOBY's state seminars are three-day conferences meant to teach high school
+                            sophomores how to use and expand on their skills, personalities, and passions
+                            to lead and help others. Ambassadors are split into groups facilitated by two
+                            student facilitators and an adult mentor. In this position, I:
                             <ul>
-                                <li>Communicated information regarding RPC events to students at
-                                    Hanszen.</li>
-                                <li>Provided RPC with feedback on events and provided information
-                                    gathered from Hanszen students.</li>
+                                <li>Assisted ambassadors in moving into their dorms, goal-setting, and learning
+                                    about how to become better leaders.</li>
+                                <li>Showed ambassadors how to practice HOBY's core values through engaging
+                                    them in various leadership activities based on the HOBY curriculum.</li>
+                                <li>Motivated ambassadors to step out of their comfort zones by encouraging
+                                    them and creating a safe space for exploration and vulnerability.</li>
+                                <li>Encouraged students from my high school to attend the seminar and grow their
+                                    leadership potential.</li>
                             </ul>
                         </p>
                     </div>
