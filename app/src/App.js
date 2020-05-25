2c7562c128e8b3aa2dc3e7716805a7aea2f9edb9
chaballen <chab.a.allen@gmail.com>
2020-05-24 20:14:28 -0500

    added custom redirect

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route} from 'react-router-dom'
 import Main from './main/main';
 import Microsoft from './about/work/microsoft';
 import Rice from './about/work/rice';
@@ -18,23 +18,21 @@ class App extends Component {
         return (
             <div className={'container'}>
                 <BrowserRouter>
-                    <div className={'pages'}>
-                        <Route exact path={'/'} component={Main}/>
+                    <Route exact path={'/'} component={Main}/>
 
-                        <Route exact path={'/about'} component={About}/>
-                        <Route exact path={'/microsoft'} component={Microsoft}/>
-                        <Route exact path={'/rice'} component={Rice}/>
-                        <Route exact path={'/hanszen'} component={Hanszen}/>
-                        <Route exact path={'/hoby'} component={Hoby}/>
+                    <Route path={'/about'} component={About}/>
+                    <Route path={'/microsoft'} component={Microsoft}/>
+                    <Route path={'/rice'} component={Rice}/>
+                    <Route path={'/hanszen'} component={Hanszen}/>
+                    <Route path={'/hoby'} component={Hoby}/>
 
-                        <Route exact path={'/portfolio'} component={Portfolio}/>
-                        <Route exact path={'/resume'} component={Resume}/>
-                        <Route exact path={'/cs'} component={CS}/>
-                        <Route exact path={'/art'} component={Art}/>
-                        <Route exact path={'/reli'} component={Reli}/>
+                    <Route exact path={'/portfolio'} component={Portfolio}/>
+                    <Route exact path={'/resume'} component={Resume}/>
+                    <Route exact path={'/cs'} component={CS}/>
+                    <Route exact path={'/art'} component={Art}/>
+                    <Route exact path={'/reli'} component={Reli}/>
 
-                        <Route exact path={'/contact'} component={Contact}/>
-                    </div>
+                    <Route exact path={'/contact'} component={Contact}/>
                 </BrowserRouter>
             </div>
         );
